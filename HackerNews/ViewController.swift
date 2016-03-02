2eb24ca78a72a1980eeeb27bb17dd39adc7f1c6f
Wen Niao Qu <wqu1202@gmail.com>
2016-03-01 22:58:43 -0500

    Initial Commit

---
+++ HackerNews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HackerNews
+//
+//  Created by Wen Niao Qu on 3/1/16.
+//  Copyright (c) 2016 Wen Niao Qu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
